b7f183cec37038ff0deb7324ab069cdbfd3936b2
Matteo <124249718+Teooo06@users.noreply.github.com>
2024-02-29 16:09:53 +0100

    Update MainApp.java
    Traiettoria frutta e generatore casuale frutta

--- src/main/MainApp.java
+++ src/main/MainApp.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.ArrayList;
+
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -7,21 +9,13 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import main.controller.LongValue;
-import main.controller.MainController;
 import main.controller.Sprite;
 
 public class MainApp extends Application {
     private int dimX = 1000;
     private int dimY = 600;
 
-    private MainController mainController;
-    private int secondsElapsed = 0;
-    private AnimationTimer timer;
-    private long lastNanoTime;
-    private boolean running = false;
     private GraphicsContext gc;
 
     @Override
@@ -33,7 +27,6 @@ public class MainApp extends Application {
         loader.setLocation(MainApp.class.getResource("guiFolder/mainGui.fxml"));
 
        
-        StackPane pane=  (StackPane) loader.load();
         Canvas canvas = new Canvas(dimX, dimY);
         
         //Group root = new Group(canvas,pane);
@@ -48,11 +41,8 @@ public class MainApp extends Application {
 
         primaryStage.show();
 
-        mainController = loader.getController();
-        mainController.setMainApp(this);
 
 
-        LongValue lastNanoTime = new LongValue( System.nanoTime() );
 
         // Imposto lo sfondo
         Sprite sfondo = new Sprite();
@@ -60,100 +50,68 @@ public class MainApp extends Application {
         sfondo.setPosition(0, 0);
         sfondo.setVelocity(0, 0);
 
+        // Mela
         Sprite mela = new Sprite();
         mela.setImage("main/images/apple.png");
-        mela.setPosition(dimX / 2, dimY - 80);
-        mela.setVelocity(100, -100);
-        
+        mela.setPosition(300, dimY+100);
+        mela.setVelocity(100, 0);
+        mela.setVelocitaIniziale(-9); // Range da -12 a -9.5
+
+
+        // Crea un arraylist per gestire i frutti
+        ArrayList<Sprite> elencoFrutta = new ArrayList<Sprite>();
+
+
         // nuova animazione
         new AnimationTimer() {
+            double lastNanoTime = System.nanoTime();
+            
             public void handle(long currentNanoTime) {
-                double elapsedTime = (currentNanoTime - lastNanoTime.value) / 1000000000.0;
-                lastNanoTime.value = currentNanoTime;
+                double elapsedTime = (currentNanoTime - lastNanoTime) / 1000000000.0;
+                lastNanoTime = currentNanoTime;
                 
                 gc.clearRect(0, 0, dimX, dimY);
-                mela.update(elapsedTime);
                 sfondo.render(gc);
-                mela.render(gc);
+                
+                
+                // controllo generatore Frutti
+                int genera = (int) (Math.random() * 50);
+                
+                if (genera == 3) {
+                    double tempoIniziale = System.currentTimeMillis();
+                    Sprite frutto = new Sprite();
+                    frutto.setTempoIniziale(tempoIniziale);
+                    frutto.setImageRandom();
+                    double px = dimX * Math.random()+50;
+                    double py = dimY +100;
+                    frutto.setPosition(px, py);
+                    // Può andare a destra o a sinistra
+                    int i= (int) (Math.random() * 2);
+                    if(i==0)
+                    frutto.setVelocity(-Math.random()*100, 0);
+                    else
+                    frutto.setVelocity(Math.random()*100, 0);
+                    double velocitaIniziale = -9.5 + (int) (Math.random() * -2.5);
+                    frutto.setVelocitaIniziale(velocitaIniziale); // Range da -12 a -9.5
+                    elencoFrutta.add(frutto);
+                }
+                
+                //double tempoTrascorso = (System.currentTimeMillis() - tempoIniziale) / 1000.0;
+
+
+                // movimento frutto
+                for (Sprite frutto : elencoFrutta) {
+                    frutto.update(elapsedTime);
+                    frutto.render(gc);
+                }
             }
         }.start();
 
-
-        /*startTimer();
-        scene.setOnKeyPressed(event -> {
-            switch (event.getCode()) {
-                case P:
-                    if (timer != null) {
-                        if (running) {
-                            stopTimer();
-                            running = false;
-                        } else {
-                            startTimer();
-                            running = true;
-                        }
-                    } else {
-                        startTimer();
-                        running = true;
-                    }
-                    break;
-                case R:
-                    resume();
-                    break;
-                default:
-                    break;
-            }
-        });*/
         } catch (Exception e) {
         e.printStackTrace();
     }
     }
 
-    /*public void restartTimer() {
-        secondsElapsed = 0;
-        stopTimer();
-        startTimer();
-    }
-
-    //public void startTimer() {
-        lastNanoTime = System.nanoTime();
-        timer = new AnimationTimer() {
-            @Override
-            public void handle(long currentNanoTime) {
-                double elapsedTime = (currentNanoTime - lastNanoTime) / 1e9;
-                lastNanoTime = currentNanoTime;
-                updateTimer(elapsedTime);
-
-                gc.clearRect(0, 0, dimX, dimY);
-
-                mela.update(elapsedTime);
-                mela.render(gc);
-            }
-        };
-        timer.start();
-    }
-
-    public void stopTimer() {
-        if (timer != null) {
-            timer.stop();
-        }
-    }
-
-    public void resume() {
-        if (!running) {
-            startTimer();
-            running = true;
-        }
-    }
-
-    private void updateTimer(double elapsedTime) {
-        secondsElapsed += elapsedTime;
-        int cents = (int) (secondsElapsed / 0.6) % 100;
-        int seconds = (int) (secondsElapsed / 60) % 60;
-        int minutes = (int) secondsElapsed / 3600;
-        String formattedTime = String.format("%02d:%02d:%02d", minutes, seconds, cents);
-        mainController.updateTimerLabel(formattedTime);
-    }
-*/
     public static void main(String[] args) {
         launch(args);
     }
